26c4c0efb9a9e678e5882f1d41d966abe6ec7ce8
Moghal Akram Baig <moghalakrambaig8@gmail.com>
2025-11-17 13:12:28 +0530

    railway ort

--- src/main/java/com/spiritedhub/spiritedhub/config/SecurityConfig.java
+++ src/main/java/com/spiritedhub/spiritedhub/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(List.of("http://localhost:5173"));
+                    config.setAllowedOrigins(List.of("http://localhost:5173", "https://beverageking.vercel.app"));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("*"));
                     config.setAllowCredentials(true);
