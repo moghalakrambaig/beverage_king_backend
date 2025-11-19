48e9218f60c5b9aa33fe238925d44226352a137d
Moghal Akram Baig <moghalakrambaig8@gmail.com>
2025-11-19 08:52:44 +0530

    push back

--- src/main/java/com/spiritedhub/spiritedhub/service/EmailService.java
+++ src/main/java/com/spiritedhub/spiritedhub/service/EmailService.java
@@ -19,39 +19,3 @@ public class EmailService {
         mailSender.send(message);
     }
 }
-
-// package com.spiritedhub.spiritedhub.service;
-
-// import com.resend.Resend;
-// import com.resend.services.emails.Emails;
-// import com.resend.services.emails.model.CreateEmailOptions;
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.stereotype.Service;
-
-// @Service
-// public class EmailService {
-
-//     @Value("${resend.api.key}")
-//     private String apiKey;
-
-//     @Value("${resend.from.email}")
-//     private String fromEmail;
-
-//     public void sendEmail(String to, String subject, String message) {
-//         try {
-//             Resend resend = new Resend(apiKey);
-
-//             CreateEmailOptions request = CreateEmailOptions.builder()
-//                     .from(fromEmail)
-//                     .to(to)
-//                     .subject(subject)
-//                     .html("<p>" + message + "</p>")
-//                     .build();
-
-//             resend.emails().send(request);
-
-//         } catch (Exception e) {
-//             throw new RuntimeException("Email failed: " + e.getMessage());
-//         }
-//     }
-// }
