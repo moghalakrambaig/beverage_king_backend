e3b679ed313e998a44791ec31eeaf19de1f5b1b3
Moghal Akram Baig <moghalakrambaig8@gmail.com>
2025-12-04 11:31:53 +0530

    Backend Changes v3

--- src/main/java/com/spiritedhub/spiritedhub/config/SesConfig.java
+++ src/main/java/com/spiritedhub/spiritedhub/config/SesConfig.java
@@ -3,7 +3,6 @@ package com.spiritedhub.spiritedhub.config;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,15 +11,6 @@ import software.amazon.awssdk.services.ses.SesClient;
 
 @Configuration
 public class SesConfig {
-
-        @Value("${AWS_ACCESS_KEY_ID}")
-        private String accessKey;
-
-        @Value("${AWS_SECRET_ACCESS_KEY}")
-        private String secretKey;
-
-        @Value("${AWS_REGION}")
-        private String region;
 
         @Bean
         public SesClient sesClient() {
